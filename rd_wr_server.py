417bc3c31529312d7a41247416db813a9b20134b
Finaritra Razafimahazo <finaritra.razaf@yahoo.fr>
2018-03-25 21:07:35 +0000

    Add files via upload

--- rd_wr_server.py
+++ rd_wr_server.py
@@ -9,12 +9,12 @@ import requests
 #read from server
 r = requests.get('https://apfflashline.herokuapp.com/institutions/5a1ad023873e2b14c0f2d172/buildings/5a1ad023873e2b14c0f2d17b')
 data_req = r.json()
-
-print ("Using python requests library: " + str(data_req))
-
-
-
-
+print('')
+print ("Using python requests library: ")
+print('')
+print ("Number being served: 10 " )
+print ("Next ticket available " + str(data_req['actualNumber']))
+print('')
 reader = SimpleMFRC522.SimpleMFRC522()
 
 try:
@@ -23,16 +23,23 @@ try:
 	id, text = reader.read()
 	r_2 = requests.post('https://apfflashline.herokuapp.com/institutions/5a1ad023873e2b14c0f2d172/buildings/5a1ad023873e2b14c0f2d17b', json={"actualNumber": "5"})
         data_postreq = r_2.json()
-        print ("Updated ticket number: " + str(data_postreq))
+        print ("Ticket picked ")
+        time.sleep(1)
+        print ("Number being served: 10 " )
+        print ("Next ticket available " + str(data_postreq))
+
+        time.sleep(1)
         print("-------------------------------------------------")
 	print('This is the ID of the data: ', id)
 	print('This is the data in the card: ', text)
         print("-------------------------------------------------")
         time.sleep(3)
+	
 	print ("Now place your tag to write the ticket number")
 	reader.write(str(data_postreq))
 	print("Written")
 	time.sleep(3)
+	
 	print("Place your tag to check the data in the card")
 	id1, text1 = reader.read()
 	print("-------------------------------------------------")
